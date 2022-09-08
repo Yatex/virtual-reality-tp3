02ed28aeff9c724a845a7997cb1a67af60461faa
lgalende <lgalende@itba.edu.ar>
2022-09-08 18:40:10 -0300

    Lighting and thunder

---
+++ Assets/Scripts/LightingController.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightingController : MonoBehaviour
+{
+    public GameObject lighting1;
+    public GameObject lighting2;
+    public GameObject lighting3;
+
+    public GameObject audio;
+
+    void Start()
+    {
+        EndLighting();
+        EndThunder();
+        Invoke("StartLighting", 4.5f);   
+    }
+
+    void StartLighting()
+    {
+        int r = Random.Range(0, 3);
+        switch (r)
+        {
+            case 0:
+                lighting1.SetActive(true);
+                Invoke("EndLighting", .75f);    // in 75 ms it will call EndLighting 
+                Invoke("StartThunder", .100f);  // in 200 ms it will call StartThunder   
+                break;
+            case 1:
+                lighting2.SetActive(true);   
+                Invoke("EndLighting", .100f);  
+                Invoke("StartThunder", .200f);
+                break;  
+            case 2:
+                lighting3.SetActive(true);   
+                Invoke("EndLighting", .125f);  
+                Invoke("StartThunder", .400f);  
+                break;
+            default:
+                Debug.LogError("StartLighting error");
+                break;
+        }
+    }
+
+    void EndLighting()
+    {
+        lighting1.SetActive(false);   
+        lighting2.SetActive(false);   
+        lighting3.SetActive(false);
+
+        float rand = Random.Range(10f, 12f);
+        Invoke("StartLighting", rand);
+    }
+
+    void StartThunder()
+    {
+        audio.SetActive(true);
+        Invoke("EndThunder", 3.5f);
+    }
+
+    void EndThunder()
+    {
+        audio.SetActive(false);
+    }
+
+    void Update()
+    {
+        
+    }
+}
