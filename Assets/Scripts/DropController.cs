f5c0b6648e8da6f190332ee986fd78bdaa72c7f8
mtarradellas <mtarradellas@itba.edu.ar>
2022-09-08 18:23:33 -0300

    drop prefab changes

--- Assets/Scripts/DropController.cs
+++ Assets/Scripts/DropController.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class DropController : MonoBehaviour {
     [SerializeField] private float lowerLimit = 1.1f;
+    [SerializeField] private List<int> ignoreLayers;
     private float lifetime = 5f;
     
     void Update() {
         lifetime -= Time.deltaTime;
         if (transform.position.y <= lowerLimit || lifetime == 0) {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void OnTriggerEnter(Collider collider) {
+        if (!ignoreLayers.Contains(collider.gameObject.layer)) {
             Destroy(this.gameObject);
         }
     }
